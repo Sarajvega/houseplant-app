8193cabdafdd99fc05a389b4234bde05bb368ad4
Sara <sarajessvega17@gmail.com>
2021-08-12 17:17:30 -0500

    pwa settings re-added

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 // Can I configure this to only run when I produce my app?
 // doesn't actually affect development.
 
-// const withPWA = require('next-pwa')
-// const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: 'public',
-//     runtimeCaching,
-//   },
-// })
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
