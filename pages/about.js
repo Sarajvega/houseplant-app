56c428104553d92ab306886df6096b393433f135
Sara <sarajessvega17@gmail.com>
2021-08-16 12:02:47 -0500

    about updated

--- pages/about.js
+++ pages/about.js
@@ -6,11 +6,10 @@ export default function About() {
         <h1>
             About
         </h1>
-        <h3>What is this for?</h3>
+        <h3>What is this app for?</h3>
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet laoreet lectus ac auctor. Vivamus eros nisi, vulputate non nisl vitae, dignissim convallis orci. Duis a lorem vel mi molestie vulputate sed nec turpis. Vestibulum id nisi in ante auctor gravida. Suspendisse cursus nisl purus, at congue nibh ultrices non. Phasellus et mauris sed elit malesuada aliquam. Morbi id feugiat tortor, quis finibus mauris. Proin euismod turpis lacus, sed congue neque mollis sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-
-            Phasellus auctor purus a lobortis volutpat. Nunc vulputate arcu in orci scelerisque, eu consectetur ex auctor. Praesent ac lorem justo. Aenean nulla nunc, semper nec volutpat et, suscipit in mi. Suspendisse laoreet sodales diam eget malesuada. Curabitur sit amet cursus nibh. Suspendisse neque est, cursus eget ante eu, fermentum lacinia massa. Aenean consequat ultricies ipsum, id fringilla leo placerat vel. 
+            The purpose of this app is to help identify house plants for the budding horticultural enthusiast! 
+            Learning your first plants can be a challenge. Houseplant.app seeks to make the transition between plant id newbie and veteran easy as possible. We also seek to provide you the essential information for caring for your new houseplant and ensuring it will thrive! 
         </p>
     </div>)
 }
